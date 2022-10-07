65f2de410006a966759334391ad24bec582f5509
Alexandros Kourmoulakis <pro_balla2000@hotmail.com>
2022-10-07 10:43:23 +0300

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,13 @@
+<div align="center">
+ <h1>Compounding Calculator</h1>
+ </div>
+
+ Site: https://akvrmvlakis-compounding.netlify.app/
+
+This small app is made in order to calculate compounded profit whether it is trading or betting gains or mortgage payments you name it.
+
+## Instructions
+
+1. Set your initial balance.
+2. Choose a rate e.g 1.01 is 1% and 1.99 is 99%.
+3. Set the time interval e.g this could be after how many months you want to see your gains compounded or after how many rounds or how many bets in a parlay configuration etc.
---
+++ scripts.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const compound = function () {
+  const value1 = document.getElementById("value1").value;
+  const value2 = document.getElementById("value2").value;
+  const value3 = document.getElementById("value3").value;
+  const compResult = (value1 * value2 ** value3).toFixed(2); 
+  document.querySelector(".result").innerText = compResult;
+  return compResult;
+};
+
+const result = document.querySelector(".result");
+const calcButton = document.querySelector(".calculate");
+calcButton.addEventListener("click", () => {
+  compound();
+});
+const resetButton = document.querySelector(".reset");
+resetButton.addEventListener("click", () => {
+  result.classList.add("hidden");
+});
+const resetFields = document.querySelector(".reset");
+resetFields.addEventListener("click", () => {
+  location.reload();
+});
