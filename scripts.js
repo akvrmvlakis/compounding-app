f3b757e31bbdc22c6e31f09f4b06ca8482aa4487
Alexandros Kourmoulakis <pro_balla2000@hotmail.com>
2023-11-16 11:36:24 +0200

    improved reset button functionality

--- scripts.js
+++ scripts.js
@@ -1,26 +1,27 @@
 "use strict";
 
-const compound = function () {
-  const value1 = document.getElementById("value1").value;
-  const value2 = document.getElementById("value2").value;
-  const value3 = document.getElementById("value3").value;
-  const compResult = (value1 * value2 ** value3).toFixed(2);
-  document.querySelector(".result").innerText = compResult;
-  return compResult;
+const calculateCompound = () => {
+  const getValue = (id) => Number(document.getElementById(id).value);
+  const value1 = getValue("value1");
+  const value2Percentage = getValue("value2") / 100;
+  const value3 = getValue("value3");
+  const result = (value1 * (1 + value2Percentage) ** value3).toFixed(2);
+  const resultElement = document.querySelector(".result");
+  resultElement.innerText = result;
+  resultElement.classList.remove("hidden"); // Ensure the result is visible
+  return result;
 };
 
-const result = document.querySelector(".result");
-const calcButton = document.querySelector(".calculate");
-calcButton.addEventListener("click", () => {
-  compound();
-});
-const resetButton = document.querySelector(".reset");
-resetButton.addEventListener("click", () => {
-  result.classList.add("hidden");
-});
-const resetFields = document.querySelector(".reset");
-resetFields.addEventListener("click", () => {
-  location.reload();
-});
+const resetCalculator = () => {
+  const resultElement = document.querySelector(".result");
+  resultElement.classList.add("hidden");
+  document.getElementById("value1").value = "0";
+  document.getElementById("value2").value = "0";
+  document.getElementById("value3").value = "0";
+  calculateCompound(); // Recalculate after resetting
+};
 
-document.getElementById("year").innerHTML = new Date().getFullYear();
+document
+  .querySelector(".calculate")
+  .addEventListener("click", calculateCompound);
+document.querySelector(".reset").addEventListener("click", resetCalculator);
